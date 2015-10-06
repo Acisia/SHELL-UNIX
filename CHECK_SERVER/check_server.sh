64d18c6c8b73cb9e4d73651d22da3e9e3e0029c0
ldaubignard <louis.daubignard@kertios.com>
2015-10-06 09:54:14 +0200

    [CHECK_SERVER] Ajout check et IP

--- CHECK_SERVER/check_server.sh
+++ CHECK_SERVER/check_server.sh
@@ -11,58 +11,58 @@
 ###################################################################################################################
 #CHEMIN RACINE
 PATHROOT="$PWD"
+NOMPROJECTSCRIPT="CHECK SERVEUR LINUX"
 
 #RECUPERATION DES FONCTIONS
-. $PATHROOT/../lib/functions.sh
+. "$PATHROOT/../lib/functions.sh"
 
+printMessageTo  "             $NOMPROJECTSCRIPT										" "1" 
+
+##################################################################################################################
 #RECUPERATION DES PROPERTIES
-. $PATHROOT/../config/config.sh
-
-
-echo  "--------------------------------------------------------------------------------------------------"
-echo  "                   CHECK SERVEUR LINUX							"
-echo  "--------------------------------------------------------------------------------------------------"
+checkPathFile "$PATHROOT/../config/config.sh" "Fichier de config"
+if [ $? -eq 0 ];then
+	echo -e "\033[31m[ERREUR]\033[0m Manque fichier de config"
+	exit 1
+fi
+. "$PATHROOT/../config/config.sh"
+#CHECK DES LOGS
+checkPathDst "$PATHDEST_REPLOG" "Chemin LOG"
 
 
 ##################################################################################################################
 # TEST PRE-REQUIS
+printMessageTo  "             CONTROLE APPLI										" "3" 
 # Vérifier que l'utilisateur est root
 checkUserRoot
 # Vérifier que perl est installé pour la gestion des mots de passe
 checkAppli perl
 ##################################################################################################################
 
-#--------------------------------------------------------------------------------------------------
-#                                           ETAT SYSTEME
-#--------------------------------------------------------------------------------------------------
-echo  "--------------------------------------------------------------------------------------------------"
-echo  "                   ETAT SYSTEME  "
-echo  "--------------------------------------------------------------------------------------------------"
-echo  " HOSTNAME"
-echo  "------------"
+##################################################################################################################
+# DEBUT PROCESS
+# Vérifier que l'utilisateur est root
+printMessageTo  "    PROCESS $NOMPROJECTSCRIPT START	 " "3" 
+
+printMessageTo  "    HOSTNAME	 " "2"
 hostname
 
-echo ""
-echo "------------"
-echo " OS"
-echo "------------"
+printMessageTo  "    OS	 " "2"
 cat /etc/lsb-release
 
-echo ""
-echo "------------"
-echo " ARCHITECTURE"
-echo "------------"
+printMessageTo  "    ARCHITECTURE	 " "2"
 uname -i
 
-echo ""
-echo "------------"
-echo " Espace disque"
-echo "------------"
+printMessageTo  "    ESPACE DISQUE	 " "2"
 df -h
 
-echo ""
-echo "------------"
-echo " Disque"
-echo "------------"
+printMessageTo  "    LISTE DES INTERFACES RESEAUX	 " "2"
+ifconfig | perl -nle 's/dr:(\S+)/print $1/e'
+
+printMessageTo  "    ADRESSE IP UP	 " "2"
+INTERADDRIP=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+echo $INTERADDRIP
+
+printMessageTo  "    LISTER LES DISQUES	 " "2"
 sudo fdisk -l
 
