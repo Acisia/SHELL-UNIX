41d814ab5ccca0ba5c028afc125dea00dafee4db
ldaubignard <louis.daubignard@kertios.com>
2015-10-07 11:07:31 +0200

    [INSTALL_APPLI] Creation script install svox

--- INSTALL_APPLI/svox.sh
+++ INSTALL_APPLI/svox.sh
@@ -1,19 +1,117 @@
+#!/bin/sh
+#######################################################################
+# Author : Louis DAUBIGNARD
+# Date   : 07/10/2015
+#
+# Description : Script pour :
+#		- Installation application SVOX
+#
+# Syntax : [application].sh 
+#
+########################################################################
+# Sources web :
+#	http://forum.ubuntu-fr.org/viewtopic.php?id=108430
+#	http://blog.erwan.me/post/2015/05/06/Synth%C3%A8se-vocale-sous-Ubuntu-avec-SVOX-pico-TTS
+#	http://elinux.org/RPi_Text_to_Speech_%28Speech_Synthesis%29#Pico_Text_to_Speech
+#	http://wiki.freeswitch.org/wiki/Mod_tts_commandline
+########################################################################
+#CHEMIN RACINE
+PATHROOT="$PWD"
+###########
+clear
+############################ Traduction ##############################
+#
+install_msg_fr() {
+    msg_installer_welcome="*      Bienvenue dans l'assistant d'intallation/mise à jour de SVOX Pico       *"
+	msg_installer_name="*      Installation SVOX Pico       *"
+	msg_file_config="\033[31m[ERREUR]\033[0m Manque fichier de config"
+	msg_ctrl_system="\033[35m[00]\035[0m Contrôle - Pre-requis"
+	msg_install_multiverse="\033[35m[01]\035[0m Installation Dépôts multiverse"
+	msg_install_dependency="\033[35m[02]\035[0m Mise à jour du système"
+	msg_install_dependency="\033[35m[03]\035[0m Installation des dépendances"
+	msg_install_test="\033[35m[FIN]\035[0m Test de l'application"
+	
+}
+install_msg_en() {
+    msg_installer_welcome="*      Welcome to SVOX Pico installer/updater        *"
+	msg_installer_name="*      SVOX Pico Installer      *"
+	msg_file_config="\033[31m[ERROR]\033[0m Config file is missing"
+	msg_ctrl_system="\033[35m[00]\035[0m Control - Pre-requisite"
+	msg_install_multiverse="\033[35m[01]\035[0m Installation Dépôts multiverse"
+	msg_install_dependency="\033[35m[02]\035[0m Mise à jour du système"
+	msg_install_dependency="\033[35m[03]\035[0m Installation des dépendances"
+	msg_install_test="\033[35m[FIN]\035[0m Test de l'application"
+}
+install_msg_pt() {
+    msg_installer_welcome="*      Bem-vindo ao assistente de instalação / atualização SVOX Pico        *"
+	msg_installer_name="*      SVOX Pico instalação      *"
+	msg_file_config="\033[31m[ERROR]\033[0m Config file is missing"
+	msg_ctrl_system="\033[35m[00]\035[0m Controle - Pré-requisito"
+	msg_install_multiverse="\033[35m[01]\035[0m Multiverse source install"
+	msg_install_dependency="\033[35m[02]\035[0m System Update"
+	msg_install_dependency="\033[35m[03]\035[0m Dependency install"
+	msg_install_test="\033[35m[FIN]\035[0m Test"	
+}
+install_msg_de() {
+    msg_installer_welcome="*       Willkommen beim SVOX Pico Installer / Updater       *"
+	msg_installer_name="*      SVOX Pico Installer      *"
+	msg_file_config="\033[31m[ERROR]\033[0m Config file is missing"
+	msg_ctrl_system="\033[35m[00]\035[0m Steuerung - Erforderliche"
+	msg_install_multiverse="\033[35m[01]\035[0m Multiverse source install"
+	msg_install_dependency="\033[35m[02]\035[0m System Update"
+	msg_install_dependency="\033[35m[03]\035[0m Dependency install"
+	msg_install_test="\033[35m[FIN]\035[0m Test"
+}
+############################ Fonctions ##############################
+#
+setup_i18n() {
+    lang=${LANG:=en_US}
+    case ${lang} in
+        [Ff][Rr]*)
+            install_msg_fr
+        ;;
+        [Ee][Nn]*|*)
+            install_msg_en
+        ;;
+		[Pt][Pt]*|*)
+            install_msg_pt
+        ;;
+        [De][De]*|*)
+            install_msg_de
+        ;;
+    esac
+}
+install_srcmultiverse() {
+	echo "deb http://fr.archive.ubuntu.com/ubuntu/ trusty universe multiverse" | sudo tee /etc/apt/sources.list.d/multiverse.list
+	echo "deb http://security.ubuntu.com/ubuntu trusty-security universe multiverse" 	>> /etc/apt/sources.list.d/multiverse.list
+	echo "deb http://fr.archive.ubuntu.com/ubuntu/ trusty-updates universe multiverse" 	>> /etc/apt/sources.list.d/multiverse.list
+}
+install_dependency() {
+    apt-get update
+    apt-get -y install
+    apt-get -y install build-essential
+    apt-get -y install curl
+	apt-get -y install git-core 
+	apt-get -y install automake 
+	apt-get -y install autogen 
+	apt-get -y install libtool 
+	apt-get -y install libpopt-dev
+	apt-get -y install xclip
+	apt-get -y install alsa-utils
+	apt-get -y install libttspico-utils
+	apt-get -y install zenity
+	apt-get -y install xsel
 
-#http://forum.ubuntu-fr.org/viewtopic.php?id=108430
-#http://blog.erwan.me/post/2015/05/06/Synth%C3%A8se-vocale-sous-Ubuntu-avec-SVOX-pico-TTS
-#http://elinux.org/RPi_Text_to_Speech_%28Speech_Synthesis%29#Pico_Text_to_Speech
-# http://wiki.freeswitch.org/wiki/Mod_tts_commandline
-sudo apt-get install -y build-essential git-core automake autogen libtool libpopt-dev
-sudo apt-get install -y xclip alsa-utils libttspico-utils zenity xsel
-
-#compilation 
-cd /usr/src
-git clone git://git.debian.org/collab-maint/svox.git svox-git
-cd svox-git
-git branch -a
-git checkout -f origin/debian-sid
-cd pico
-base64 -d -i << EOF | patch -p2
+    apt-get autoremove
+}
+install_software() {
+	cd /usr/src
+	git clone git://git.debian.org/collab-maint/svox.git svox-git
+	cd svox-git
+	git branch -a
+	git checkout -f origin/debian-sid
+	cd pico
+	base64 -d -i << EOF | patch -p2
 ZGlmZiAtLWdpdCBhL3BpY28vbGliL3BpY29hcGkuYyBiL3BpY28vbGliL3BpY29hcGkuYwppbmRl
 eCBiZTZkMWEyLi5kMDk5YmRhIDEwMDY0NAotLS0gYS9waWNvL2xpYi9waWNvYXBpLmMKKysrIGIv
 cGljby9saWIvcGljb2FwaS5jCkBAIC00MSwxMCArNDEsMTAgQEAgZXh0ZXJuICJDIiB7CiAjZGVm
@@ -113,13 +211,52 @@ ICAgICAgICAgICBpZiAocmVtID4gMCkgewogICAgICAgICAgICAgICAgIHJlcy0+c3RhcnQgPSBy
 ZXMtPnJhd19tZW0gKyAoUElDT09TX0FMSUdOX1NJWkUgLSByZW0pOwogICAgICAgICAgICAgfSBl
 bHNlIHsK
 EOF
-./autogen.sh
-./configure --prefix=/opt/svox-pico/
-make install
-#Télécharger et lancer le script:
-wget http://liveusb.info/xclip-speech/svox_pico.sh
-chmod +x svox_pico.sh
-./svox_pico.sh
+	./autogen.sh
+	./configure --prefix=/opt/svox-pico/
+	make install
+}
+test_software() {
+	#Télécharger et lancer le script:
+	wget http://liveusb.info/xclip-speech/svox_pico.sh
+	chmod +x svox_pico.sh
+	./svox_pico.sh
+	pico2wave -l fr-FR -w test.wav "le même test avec la voix feminine .f r de M brola et espeak"
+	aplay test.wav
+}
+########################## Main Point entree ############################
+# DEBUT ENTREE
+setup_i18n
+############################ Lib Fonctions ##############################
+#
+. "$PATHROOT/../lib/functions.sh"
+printMessageTo  "             ${msg_installer_name}			" "1" 
 
-pico2wave -l fr-FR -w test.wav "le même test avec la voix feminine .f r de M brola et espeak"
-aplay test.wav
+########################## Fic Configuration ############################
+#
+checkPathFile "$PATHROOT/../config/config.sh" "Fichier de config"
+if [ $? -eq 0 ];then
+	echo -e "${msg_file_config}"
+	exit 1
+fi
+. "$PATHROOT/../config/config.sh"
+#CHECK DES LOGS
+checkPathDst "$PATHDEST_REPLOG" "Chemin LOG"
+##################################################################################################################
+# TEST PRE-REQUIS
+# Vérifier que l'utilisateur est root
+printMessageTo  "             CONTROLE APPLI			" "3" 
+checkUserRoot
+# Vérifier que perl est installé pour la gestion des mots de passe
+checkAppli perl
+##################################################################################################################
+# DEBUT PROCESS
+printMessageTo  "    ${msg_installer_name}	 " "3" 
+printMessageTo  "    ${msg_install_multiverse}	 " "2" 
+install_srcmultiverse
+printMessageTo  "    ${msg_install_dependency}	 " "2"
+install_dependency
+printMessageTo  "    ${msg_install_test}	 " "2" 
+test_software
+
+
+
